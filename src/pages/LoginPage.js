f57a9ee4e29215146d15da2622fea45983bc126c
Karlo Sintic <karlo.sintic@gmail.com>
2020-04-29 21:42:00 +0200

    Created Login UI

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import {
-  IonText,
   IonContent,
   IonHeader,
-  IonToolbar,
-  IonTitle,
   IonPage,
   IonButton,
+  IonCard,
+  IonItem,
+  IonInput,
+  IonList,
+  IonCardHeader,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
 import { withRouter, Redirect } from "react-router";
+
+import "./LoginPage.css";
 
 import * as db from "../db";
 
@@ -50,13 +56,40 @@ const LoginPage = (props) => {
     return (
       <IonPage>
         <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login / Sign Up</IonTitle>
-          </IonToolbar>
+          <IonRow>
+            <IonCol className="col1"></IonCol>
+            <IonCol className="col2"></IonCol>
+            <IonCol className="col3"></IonCol>
+            <IonCol className="col4"></IonCol>
+          </IonRow>
         </IonHeader>
-        <Ion-Content>
-          <IonButton onClick={handleLogin}>Click on me to Login</IonButton>
-        </Ion-Content>
+        <IonContent>
+          <h2 className="heading2">Bok, ovo je</h2>
+          <h1 className="heading">HORA</h1>
+          <IonCard class="login-card">
+            <IonInput
+              className="login-input"
+              inputMode="email"
+              placeholder="e-mail"
+            ></IonInput>
+
+            <IonInput
+              className="login-input"
+              placeholder="password"
+              type="password"
+            ></IonInput>
+
+            <IonButton
+              fill="clear"
+              color="dark"
+              className="login-button"
+              expand="block"
+              onClick={handleLogin}
+            >
+              PRIJAVA
+            </IonButton>
+          </IonCard>
+        </IonContent>
       </IonPage>
     );
   }
